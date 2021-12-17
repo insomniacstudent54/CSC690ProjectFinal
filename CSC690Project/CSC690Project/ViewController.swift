f5db544478271b7110d41f3ddf7ede166c581afd
Kurt R <59162648+insomniacstudent54@users.noreply.github.com>
2021-12-16 19:03:20 -0800

    Add files via upload

---
+++ CSC690Project/CSC690Project/ViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ViewController.swift
+//  CSC690Project
+//
+//  Created by JPL-ST-SPRING2021 on 12/16/21.
+//  Copyright © 2021 JPL-LT-013. All rights reserved.
+//
+//tutorial followed: Swift to do list app for beginners by iOS Academy
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+
+    private let table: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    var items = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "To Do List"
+        self.items = UserDefaults.standard.stringArray(forKey: "items") ?? []
+        view.addSubview(table)
+        table.dataSource = self
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
+    }
+    
+    @objc private func didTapAdd() {
+        let alert = UIAlertController(title: "New Item", message: "Enter a new item into the list", preferredStyle: .alert)
+        alert.addTextField { field in
+            field.placeholder = "Enter item"
+        }
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Done", style: .default, handler: { [weak self](_) in
+            if let field = alert.textFields?.first{
+                if let text = field.text, !text.isEmpty {
+                    DispatchQueue.main.async {
+                        var currentItems = UserDefaults.standard.stringArray(forKey: "items") ?? []
+                        currentItems.append(text)
+                        let newEntry = [text]
+                        UserDefaults.standard.setValue(currentItems, forKey: "items")
+                        self?.items.append(text)
+                        self?.table.reloadData()
+                    }
+                    
+                }
+            }
+        }))
+        present(alert, animated: true)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        table.frame = view.bounds
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for:indexPath)
+        cell.textLabel?.text = items[indexPath.row]
+        return cell
+    }
+
+
+}
+
